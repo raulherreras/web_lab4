30a3eb5b0b7728818d10520b622a034546c06818
Raul Herrera <raul_ehs@hotmail.com>
2020-09-14 21:12:41 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let post = document.getElementById("ButtonPost");
+let clear = document.getElementById("ButtonClear");
+let mark = document.getElementById("ButtonMark");
+let del = document.getElementById("ButtonDelete");
+
+post.addEventListener("click", TodoPost);
+clear.addEventListener("click", TodoClear);
+mark.addEventListener("click", TodoMark);
+del.addEventListener("click", TodoDel);
+
+function TodoPost(e) {
+e.preventDefault(); // disable the default page reload when clicked
+
+var todo = document.getElementById("todoText").value;
+var list = document.getElementById("todoList");
+
+let currentList = list.innerHTML;
+list.innerHTML = currentList + `<input type = "checkbox" name="todo" />  ${todo}  <br>  `
+}
+
+function TodoClear(e) {
+e.preventDefault();
+}
+
+function TodoMark(e) {
+e.preventDefault();   
+}
+
+function TodoDel(e) {
+e.preventDefault();
+}
